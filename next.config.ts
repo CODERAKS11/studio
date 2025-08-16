e22cbf8866945916031ed0a92714f7dde06137c5
Amarjeet Kumar 1021 <amarjeetakskumar@gmail.com>
2025-08-16 20:00:04 +0000

    although i am getting notification in dekstop, but not in mobile

--- next.config.ts
+++ next.config.ts
@@ -9,10 +9,6 @@ const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  experimental: {
-    
-  },
-  allowedDevOrigins: ["*.cloudworkstations.dev"],
   images: {
     remotePatterns: [
       {
